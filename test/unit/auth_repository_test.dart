0eeaf698d727fba8fcacda07fd839103fa34674f
chinsuhdh <buingoctam06042003@gmail.com>
2026-07-09 23:46:16 +0700

    fix(Test): Khởi tạo TestWidgetsFlutterBinding để sửa lỗi test Auth trên CI

--- test/unit/auth_repository_test.dart
+++ test/unit/auth_repository_test.dart
@@ -1,6 +1,6 @@
 import 'package:cleanai/data/repositories/auth_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:shared_preferences/shared_preferences.dart'; 
 import '../support/dio_test_harness.dart';
 
 void main() {
